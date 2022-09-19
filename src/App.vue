c62de93cd6955aa7cc59ba659eeea1b75ba6a52c
Felipe "Felepopolo Jaré" Kuhl <99222712+felipejare@users.noreply.github.com>
2022-09-19 09:24:51 -0300

    comitando  coisa nova :)

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      cep: "",
+      resultado: {},
+    };
+  },
+  methods: {
+    async buscar() {
+      const url = `https://www.cepaberto.com/api/v3/cep?cep=$(this.cep)`;
+      this.resultado = await axios.get(url, {
+        headers: {
+          Authorization: "Token token=331b1ae99ca3314578e820ab16402077",
+        },
+      });
+    },
+  },
+};
+</script>
+<template>
+  <div class="container">
+    <h1>CEPADOR DE CASA C:</h1>
+    <form @submit.prevent="search">
+      <input placeholder="Insira o cep aqui" type="text" v-model="cep" />
+      <button type="submit">Buscar</button>
+    </form>
+  </div>
+</template>
+<style>
+.container {
+  padding: 10%;
+  background-color: rgb(88, 255, 191);
+  color: rgb(0, 0, 0);
+}
+</style>
